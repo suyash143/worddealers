bdce25814ab3fb5946c5553af334156abce5cbbb
suyash143 <suyashpathak143@gmail.com>
2022-10-28 16:02:56 +0530

    Static filkes

--- worddealers/settings.py
+++ worddealers/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
