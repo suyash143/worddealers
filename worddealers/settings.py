149850510c6561fa4bfa962212bb607cfac11dca
suyash143 <suyashpathak143@gmail.com>
2022-10-28 16:20:01 +0530

    Static filkes

--- worddealers/settings.py
+++ worddealers/settings.py
@@ -146,5 +146,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = ''
 
-'''STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage''
-'''
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage''
