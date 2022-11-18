7ec8e1748778c00a31f593f9fa7b366d66ac2b1c
suyash143 <suyashpathak143@gmail.com>
2022-11-18 20:38:39 +0530

    static

--- worddealers/settings.py
+++ worddealers/settings.py
@@ -182,5 +182,5 @@ LOGGING = {
 
 # Activate Django-Heroku.
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-django_heroku.settings(locals())
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+#django_heroku.settings(locals())
